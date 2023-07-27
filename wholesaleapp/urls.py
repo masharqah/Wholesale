9b2c2fac8cd90177b37a76f11a05876d01b0cc7d
MuntaserYounis <muntaserdagher@gmail.com>
2023-07-27 03:47:22 +0300

    edit rep, add rep , sotres display , views

--- wholesaleapp/urls.py
+++ wholesaleapp/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('home',views.home , name='mainPage'),
     path('reg', views.reg, name = 'reg'),
     path('logout', views.logout, name='logout'),
-
+    path('user_view/<int:id>',views.user_view, name = 'companyView'), #redirect to the company view page (that has company details)
+    path('add_rep',views.add_rep),
+    path('add',views.adding),
+    path('edit/<int:id>',views.edit),
+    path('edit_it',views.edit_it),
+    path('delete/<int:id>',views.delete),
 ]
--- wholesaleapp/views.py
+++ wholesaleapp/views.py
@@ -34,14 +34,23 @@ def reg(request):
         User.objects.create (
             name= request.POST['name'],
             adress= request.POST['name'],
-            phone_number= request.POST['phone_number']
+            phone_number= request.POST['phone_number'], #the comma after phone number wasn't there I added
             email= request.POST['email'],
             password=bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
             )
     return redirect ('/')
 
 def home(request):
-    pass
+    my_user = User.objects.get(id =request.session['id'] )
+    users = User.objects.all()
+    reps = Representative.objects.all()
+    context = {
+        'my_user' : my_user,
+        'users' : users,
+        'reps' : reps,
+    }
+    return render(request,'stores_display.html',context)
+
 
 # log out the site and delete all session data'
 def logout(request):
@@ -50,3 +59,34 @@ def logout(request):
         request.session['logged']= False
         request.session.flush()
     return redirect ('/')
+
+def add_rep(request):
+    return render(request,'add_rep.html')
+
+def adding(request):
+    company = User.objects.get(id = request.session['id'])
+    name = request.POST['rep_name']
+    phone_no = request.POST['phone_no']
+    email = request.POST['email']
+    city= request.POST['city']
+    Rep = Representative.objects.create(name = name,phone_no = phone_no,email = email, city=city ,company = company )
+    Rep.save()
+    return redirect('/home')
+
+def edit(request,id):
+    rep = Representative.objects.get(id =id)
+    return render(request,'add_rep.html',rep)
+
+def edit_it(request):
+    rep = User.objects.get(id = request.POST['rep_id'])
+    rep.name = request.POST['rep_name']
+    rep.phone_no = request.POST['phone_no']
+    rep.email = request.POST['email']
+    rep.city= request.POST['city']
+    rep.save()
+    return redirect('/home')
+
+def delete(request,id):
+    rep_delete = Representative.objects.get(id = id)
+    rep_delete.delete()
+    return redirect('/home')
